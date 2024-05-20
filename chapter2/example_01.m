4af2ea5aa9a7a42d5c77df5e1305776f7d3c9116
Pharino <pharino.chum@gmail.com>
2024-05-20 08:48:39 +0700

    add example

--- chapter2/example_01.m
+++ chapter2/example_01.m
@@ -1,1 +1,14 @@
 load('data1.mat');
+whos          %check variables in the workspace
+
+figure;       %create new figure to hold graphic
+imshow(ape1)  %create image from array ape1
+figure;       %create new figure to hold graphic
+imshow(img1); %create image from array img1.
+
+%Q1. what are the differnce between these two images?
+%Q2. What are the dimension of image ape1 and img1?
+
+
+
+
